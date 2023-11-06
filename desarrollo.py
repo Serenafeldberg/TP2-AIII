827c5043c5433bb8ab0d1acb4c0a93a3f35727a9
trinidad pujol <tpujolsierra@udesa.edu.ar>
2023-11-06 15:46:01 -0300

    agregue funciones

--- desarrollo.py
+++ desarrollo.py
@@ -13,13 +13,13 @@ def read_WAV_file (path):
     plt.plot(data)
     plt.xlabel("Tiempo (s) / muestras")
     plt.ylabel("Amplitud")
-    # plt.show()
+    plt.show()
 
     return data, samplerate
 
 def plot_fourier (data, samplerate):
-    dft = abs(np.fft.fft(data)) 
 
+    dft = abs(np.fft.fft(data)) 
     frecuencias = abs(np.fft.fftfreq(len(dft), 1.0 / samplerate)) 
     plt.figure(figsize=(10, 4))
     plt.plot(frecuencias, np.abs(dft), color='b')
@@ -27,9 +27,39 @@ def plot_fourier (data, samplerate):
     plt.xlabel('Frecuencia (Hz)')
     plt.ylabel('Magnitud')
     plt.grid(True)
-    # plt.show() 
+    plt.show() 
     return len(frecuencias)
 
+def plot_espectro_de_frec(data, samplerate):
+    
+    n = len(data)
+    k = np.arange(n)
+    T = n / samplerate
+    frq = k / T
+    frq = frq[range(n//2)]
+    Y = np.fft.fft(data) / n
+    Y = Y[range(n//2)]
+    plt.figure(figsize=(10, 4))
+    plt.plot(frq, abs(Y), 'r', lw=0.5)
+    plt.title('Espectro')
+    plt.xlabel('Frecuencia (Hz)')
+    plt.ylabel('|Y(freq)|')
+    plt.grid(True)
+    plt.show()
 
-data6, samplerate6 = read_WAV_file("/Users/serena/Desktop/UDESA/Analisis matematico III/TP2-AIII/mediciones_tp2/caracterizacion_fuentes/lunes/carac_fuentes_6globos.wav")
-fourier6 = plot_fourier(data6, samplerate6)
+def plot_filtro_inverso(data, samplerate):  #no se que es esto
+  
+   
+
+
+
+
+def main():
+    data6, samplerate6 = read_WAV_file("/Users/serena/Desktop/UDESA/Analisis matematico III/TP2-AIII/mediciones_tp2/caracterizacion_fuentes/lunes/carac_fuentes_6globos.wav")
+    fourier6 = plot_fourier(data6, samplerate6)
+    
+    
+
+
+if __name__ == '__main__':
+    main()
